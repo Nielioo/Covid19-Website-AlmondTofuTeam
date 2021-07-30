4de3939b303748ae3a6f2c916b7f9f6bf6b0c319
Bryan <bryanmuliawan@yahoo.co.id>
2021-07-30 17:22:56 +0700

    Update home page

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Document</title>
+  <link rel="stylesheet" href="index.css" />
+</head>
+
+<body>
+  <?php
+  require_once("navigation_bar.html");
+  ?>
+
+  <div class="container">
+    <div class="header">
+      <h1>Headline apapun yang panjang</h1>
+      <label>Tagline</label>
+      <br />
+      <button class="button"><a href="#">Next</a></button>
+    </div>
+    <div>
+      <img src="assets/family.png" />
+    </div>
+    <div class="first"></div>
+  </div>
+</body>
+
+</html>
