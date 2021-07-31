10f8fb87f789395d5c1490d6f1232507eb7a9ae7
Daniel Aprillio <danielaprillio02@gmail.com>
2021-07-31 15:11:53 +0700

    asdjadfaljsd

--- index.php
+++ index.php
@@ -71,22 +71,6 @@
       </section>
     </section>
 
-    <!-- <section class="card-goldar">
-      <div class="message">
-        &check; Made by <a href="https://codepen.io/pieter-biesemans/" target="_blank" title="&check; check out my other work">Pieter Biesemans</a> with &#9749;, &#128420; &amp; &#8986;</i>
-      </div>
-
-      <div class="card" data-tilt data-tilt-scale="0.95" data-tilt-startY="40">
-        <div class="welcome">
-          Welcome to
-        </div>
-        <div class="year">
-          <span>Twenty</span>
-          <span>Twenty</span>
-          <span>One</span>
-        </div>
-      </div>
-    </section> -->
 
   </div>
 
