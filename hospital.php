c87b39944a6cb75173dbc5d1915c46d0e3843712
Bryan <bryanmuliawan@yahoo.co.id>
2021-08-05 17:00:58 +0700

    Update search

--- hospital.php
+++ hospital.php
@@ -42,23 +42,36 @@
         $bloodtype_plus = readGolDarahByID($bloodtype_id);
         $bloodtype_negative = readGolDarahByID($bloodtype_id + 1);
     }
+
+    if (isset($_GET['submit'])) {
+        $search = $_GET['name'];
+    }
     ?>
 
     <div class="container">
         <section class="hero">
             <h1><?= $bloodtype ?></h1>
-            <label>Tagline</label>
             <div class="hero_btn">
                 <a href="hospital.php?bloodtype=<?= getGolDarahID($bloodtype_plus) - 1 ?>" class="btn"><?= $bloodtype_plus ?></a>
                 <a href="hospital.php?bloodtype=<?= getGolDarahID($bloodtype_negative) - 1 ?>" class="btn"><?= $bloodtype_negative ?></a>
             </div>
 
-            <div class="search">
-                <form onsubmit="event.preventDefault();" role="search">
-                    <label for="search">Search for stuff</label>
-                    <input id="search" type="search" placeholder="Search..." autofocus required />
-                    <button type="submit">Go</button>
+            <div class="search-box">
+                <form class="search-form" method="GET">
+                    <input type="search" name="name" value="" placeholder="Search" class="search-input" />
+                    <input type="hidden" name="bloodtype" value="<?= $bloodtype_id ?>" />
+                    <button type="submit" name="submit" class="search-button">
+                        <svg class="submit-button">
+                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#search"></use>
+                        </svg>
+                    </button>
                 </form>
+
+                <svg xmlns="http://www.w3.org/2000/svg" width="0" height="0" display="none">
+                    <symbol id="search" viewBox="0 0 32 32">
+                        <path d="M 19.5 3 C 14.26514 3 10 7.2651394 10 12.5 C 10 14.749977 10.810825 16.807458 12.125 18.4375 L 3.28125 27.28125 L 4.71875 28.71875 L 13.5625 19.875 C 15.192542 21.189175 17.250023 22 19.5 22 C 24.73486 22 29 17.73486 29 12.5 C 29 7.2651394 24.73486 3 19.5 3 z M 19.5 5 C 23.65398 5 27 8.3460198 27 12.5 C 27 16.65398 23.65398 20 19.5 20 C 15.34602 20 12 16.65398 12 12.5 C 12 8.3460198 15.34602 5 19.5 5 z" />
+                    </symbol>
+                </svg>
             </div>
         </section>
 
@@ -73,16 +86,36 @@
                     </p>
                 </div> -->
                 <?php
-                for ($i = 0; $i < sizeof($rumah_sakit_data); $i++) {
-                    $jumlah_donor = getJumlahDonorByRumahSakitID($i + 1, $bloodtype_id);
+                if (empty($_GET['name'])) {
+                    for ($i = 0; $i < sizeof($rumah_sakit_data); $i++) {
+                        $jumlah_donor = getJumlahDonorByRumahSakitID($i + 1, $bloodtype_id);
                 ?>
-                    <div class="card">
-                        <h3><?= $rumah_sakit_data[$i]['nama'] ?>, <?= $rumah_sakit_data[$i]['domisili'] ?></h3>
-                        <div class="line"></div>
-                        <p><?= $rumah_sakit_data[$i]['alamat'] ?></p>
-                        <h3>Jumlah Donor : <?= $jumlah_donor ?></h3>
-                    </div>
+                        <div class="card">
+                            <h3><?= $rumah_sakit_data[$i]['nama'] ?>, <?= $rumah_sakit_data[$i]['domisili'] ?></h3>
+                            <div class="line"></div>
+                            <p><?= $rumah_sakit_data[$i]['alamat'] ?></p>
+                            <h3>Jumlah Donor : <?= $jumlah_donor ?></h3>
+                        </div>
+                        <?php
+                    }
+                } else {
+                    for ($i = 0; $i < sizeof($rumah_sakit_data); $i++) {
+                        if (
+                            stripos($rumah_sakit_data[$i]['nama'], $search) !== false
+                            || stripos($rumah_sakit_data[$i]['alamat'], $search) !== false
+                            || stripos($rumah_sakit_data[$i]['domisili'], $search) !== false
+                        ) {
+                            $jumlah_donor = getJumlahDonorByRumahSakitID($i + 1, $bloodtype_id);
+                        ?>
+                            <div class="card">
+                                <h3><?= $rumah_sakit_data[$i]['nama'] ?>, <?= $rumah_sakit_data[$i]['domisili'] ?></h3>
+                                <div class="line"></div>
+                                <p><?= $rumah_sakit_data[$i]['alamat'] ?></p>
+                                <h3>Jumlah Donor : <?= $jumlah_donor ?></h3>
+                            </div>
                 <?php
+                        }
+                    }
                 }
                 ?>
             </div>
